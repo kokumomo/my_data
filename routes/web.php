dc423a62b871e378d76e9d717176b3f4b2ee42dc
{km} <{k3042227625n@gmail.com}>
2024-05-22 22:12:16 +0900

    140-148

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,22 @@ Route::get('/', function () {
     return view('user.welcome');
 });
 
-Route::middleware('auth:users')->group(function(){
+Route::middleware('auth:users')->group(function () {
     Route::get('/', [ItemController::class, 'index'])->name('items.index');
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-Route::prefix('cart')->middleware('auth:users')->group(function(){
+Route::prefix('cart')->middleware('auth:users')->group(function () {
     Route::get('/', [CartController::class, 'index'])->name('cart.index');
-    Route::post('add', [CartController::class, 'add'])->name('cart.add');   
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
     Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+    Route::get('checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+    Route::get('success', [CartController::class, 'success'])->name('cart.success');
+    Route::get('cancel', [CartController::class, 'cancel'])->name('cart.cancel');
 });
 
 // Route::get('/dashboard', function () {
 //     return view('user.dashboard');
 // })->middleware(['auth:users'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
