12cbe452d9bb3eff7968b06b46fb8f306d9a0ca4
{km} <{k3042227625n@gmail.com}>
2024-05-26 07:37:47 +0900

    156-158

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -13,7 +14,7 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
-        
+
         $this->middleware(function ($request, $next) {
 
             $id = $request->route()->parameter('item'); 
@@ -25,17 +26,23 @@ class ItemController extends Controller
             }
             return $next($request);
         });
-
     }
-
+        
     public function index(Request $request)
     {
+        // dd($request);
+        
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
 
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
+        ->searchKeyword($request->keyword)
         ->sortOrder($request->sort)
-        ->paginate($request->pagination ?? '20'1);
+        ->paginate($request->pagination ?? '20');
 
-        return view('user.index', compact('products'));
+        return view('user.index', 
+        compact('products', 'categories'));
     }
 
     public function show($id)
